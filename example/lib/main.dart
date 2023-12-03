cfe54512f8efc1a29b736d8c2ac27edcb208574c
SE <se@develup.pro>
2023-12-03 13:43:07 +0200

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -18,11 +18,37 @@ class WebViewApp extends StatefulWidget {
 }
 
 class _WebViewAppState extends State<WebViewApp> {
+  final workspaceConfiguration = {
+    'grid': {
+      'spacing': 20,
+      'length': 3,
+      'colour': '#ccc',
+      'snap': true,
+    },
+  };
+  final initial = {
+    "blocks": {
+      "languageVersion": 0,
+      "blocks": [
+        {
+          "type": "text",
+          "id": "Y|Ad[E=)p\$+Lu41MXB!o",
+          "x": 70,
+          "y": 30,
+          "fields": {"TEXT": "JSON"}
+        }
+      ]
+    }
+  };
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
-        child: BlocklyEditorWidget(),
+        child: BlocklyEditorWidget(
+          workspaceConfiguration: workspaceConfiguration,
+          initial: initial,
+        ),
       ),
     );
   }
